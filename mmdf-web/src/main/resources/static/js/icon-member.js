2077670270df8e9e3ff2777c4f1b195ff925c45f
peihui <pride97350@gmail.com>
2024-05-13 23:24:32 +0800

    更改連結路徑 新增人頭icon時觸發事件

--- mmdf-web/src/main/resources/static/js/icon-member.js
+++ mmdf-web/src/main/resources/static/js/icon-member.js
@@ -1,5 +1,5 @@
 // 當頁面載入時檢查登入狀態
-$(document).ready(function() {
+$(document).ready(function () {
     if (localStorage.getItem("authorization")) {
         // 如果存在驗證碼，表示已登入，顯示登出按鈕，隱藏登入按鈕
         $("#sign-in").hide();
@@ -9,6 +9,21 @@ $(document).ready(function() {
         $("#sign-in").show();
         $("#sign-out").hide();
     }
+
+    // 點擊人頭icon時觸發事件
+    $("#icon-User").click(function (e) {
+        e.preventDefault();
+        // 檢查 localstorage 中是否存在驗證碼
+        let verifyCode = localStorage.getItem("authorization");
+
+        // 如果本地存儲中存在驗證碼，則跳轉到會員資料畫面
+        if (verifyCode) {
+            window.location.href = "/member/detail";
+        } else {
+            // 如果本地存儲中不存在驗證碼，則跳轉到登入畫面
+            window.location.href = "/member/login";
+        }
+    });
 });
 
 // 登出按鈕的點擊事件
