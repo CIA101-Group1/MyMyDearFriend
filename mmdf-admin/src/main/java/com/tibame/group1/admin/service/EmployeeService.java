7d26bc92839e25f636cf6c801c5cd573df929397
Nueve <tk25683@gmail.com>
2024-05-13 09:01:39 +0800

    LoginSourceDTO 改名為Admin

--- mmdf-admin/src/main/java/com/tibame/group1/admin/service/EmployeeService.java
+++ mmdf-admin/src/main/java/com/tibame/group1/admin/service/EmployeeService.java
@@ -1,7 +1,7 @@
 package com.tibame.group1.admin.service;
 
 import com.tibame.group1.admin.dto.*;
-import com.tibame.group1.admin.dto.LoginReqDTO;
+import com.tibame.group1.admin.dto.AdminLoginReqDTO;
 import com.tibame.group1.common.dto.web.LoginResDTO;
 import com.tibame.group1.common.exception.CheckRequestErrorException;
 import com.tibame.group1.common.exception.DateException;
@@ -12,12 +12,12 @@ public interface EmployeeService {
 
     EmployeeCreateResDTO employeeCreate(EmployeeCreateReqDTO req) throws DateException, IOException;
 
-    EmployeeDetailResDTO employeeDetail(LoginSourceDTO loginSource) throws CheckRequestErrorException;
+    EmployeeDetailResDTO employeeDetail(AdminLoginSourceDTO loginSource) throws CheckRequestErrorException;
 
-    void employeeEdit(EmployeeEditReqDTO req, LoginSourceDTO loginSource)
+    void employeeEdit(EmployeeEditReqDTO req, AdminLoginSourceDTO loginSource)
             throws CheckRequestErrorException, IOException;
 
-    LoginResDTO employeeLogin(LoginReqDTO req) throws IOException;
+    LoginResDTO employeeLogin(AdminLoginReqDTO req) throws IOException;
 
 
 }
