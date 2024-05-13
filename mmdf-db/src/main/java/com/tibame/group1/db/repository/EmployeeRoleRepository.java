d32aea3f9047639311a282408a5367bbd7237833
Nueve <tk25683@gmail.com>
2024-05-13 21:53:48 +0800

    增加Querry語句

--- mmdf-db/src/main/java/com/tibame/group1/db/repository/EmployeeRoleRepository.java
+++ mmdf-db/src/main/java/com/tibame/group1/db/repository/EmployeeRoleRepository.java
@@ -1,12 +1,13 @@
 package com.tibame.group1.db.repository;
 
-import com.tibame.group1.db.entity.EmployeeEntity;
 import com.tibame.group1.db.entity.EmployeeRoleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface EmployeeRoleRepository extends JpaRepository<EmployeeRoleEntity, Integer> {
-    //通過employeeEntity查詢對應的角色
-    List<EmployeeRoleEntity> findByEmployee(EmployeeEntity employee);
+    // 根據員工id找尋角色id
+    @Query("SELECT e.employeeId FROM EmployeeEntity e WHERE e.employeeId = :employeeId")
+    List<Integer> findRoleIdsByEmployeeId(Integer employeeId);
 }
