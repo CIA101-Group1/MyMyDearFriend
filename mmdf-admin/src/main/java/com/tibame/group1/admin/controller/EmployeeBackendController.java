79c9e02529ab08059e5d64f9ebe9f0b229521182
Nueve <tk25683@gmail.com>
2024-05-13 21:48:40 +0800

    更名為AdminLoginSource

--- mmdf-admin/src/main/java/com/tibame/group1/admin/controller/EmployeeBackendController.java
+++ mmdf-admin/src/main/java/com/tibame/group1/admin/controller/EmployeeBackendController.java
@@ -3,7 +3,7 @@ package com.tibame.group1.admin.controller;
 import com.tibame.group1.admin.dto.*;
 import com.tibame.group1.admin.service.EmployeeService;
 import com.tibame.group1.common.dto.ResDTO;
-import com.tibame.group1.admin.dto.LoginReqDTO;
+import com.tibame.group1.admin.dto.AdminLoginReqDTO;
 import com.tibame.group1.common.dto.web.LoginResDTO;
 import com.tibame.group1.common.exception.CheckRequestErrorException;
 import com.tibame.group1.common.exception.DateException;
@@ -32,23 +32,23 @@ public class EmployeeBackendController {
     @PostMapping("employee/edit")
     public ResDTO<?> employeeEdit(
             @Valid @RequestBody EmployeeEditReqDTO req,
-            @RequestAttribute(LoginSourceDTO.ATTRIBUTE) LoginSourceDTO loginSource)
+            @RequestAttribute(AdminLoginSourceDTO.ATTRIBUTE) AdminLoginSourceDTO adminLoginSource)
             throws CheckRequestErrorException, IOException {
-        employeeService.employeeEdit(req, loginSource);
+        employeeService.employeeEdit(req, adminLoginSource);
         return new ResDTO<>();
     }
 
     @GetMapping("employee/detail")
     public @ResponseBody ResDTO<EmployeeDetailResDTO> employeeDetail(
-            @RequestAttribute(LoginSourceDTO.ATTRIBUTE) LoginSourceDTO loginSource)
+            @RequestAttribute(AdminLoginSourceDTO.ATTRIBUTE) AdminLoginSourceDTO adminLoginSource)
             throws CheckRequestErrorException, IOException {
         ResDTO<EmployeeDetailResDTO> res = new ResDTO<>();
-        res.setData(employeeService.employeeDetail(loginSource));
+        res.setData(employeeService.employeeDetail(adminLoginSource));
         return res;
     }
 
     @PostMapping("employee/login")
-    public @ResponseBody ResDTO<LoginResDTO> employeeLogin(@Valid @RequestBody LoginReqDTO req)
+    public @ResponseBody ResDTO<LoginResDTO> employeeLogin(@Valid @RequestBody AdminLoginReqDTO req)
             throws IOException {
         ResDTO<LoginResDTO> res = new ResDTO<>();
         res.setData(employeeService.employeeLogin(req));
