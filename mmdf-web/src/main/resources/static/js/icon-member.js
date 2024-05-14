da0b54a56ec057aee4ab915f0fd067ae4cad0ac5
peihui <pride97350@gmail.com>
2024-05-14 21:48:13 +0800

    前台：調整人頭icon內容跟連結，含行動版畫面

--- mmdf-web/src/main/resources/static/js/icon-member.js
+++ mmdf-web/src/main/resources/static/js/icon-member.js
@@ -3,11 +3,15 @@ $(document).ready(function () {
     if (localStorage.getItem("authorization")) {
         // 如果存在驗證碼，表示已登入，顯示登出按鈕，隱藏登入按鈕
         $("#sign-in").hide();
+        $("#sign-in-android").hide();
         $("#sign-out").show();
+        $("#sign-out-android").show();
     } else {
         // 如果不存在驗證碼，表示未登入，顯示登入按鈕，隱藏登出按鈕
         $("#sign-in").show();
+        $("#sign-in-android").show();
         $("#sign-out").hide();
+        $("#sign-out-android").hide();
     }
 
     // 點擊人頭icon時觸發事件
@@ -24,9 +28,29 @@ $(document).ready(function () {
             window.location.href = "/member/login";
         }
     });
+
+    // 行動版點擊人頭icon時觸發事件
+    $("#icon-User-android").click(function (e) {
+        e.preventDefault();
+        // 檢查 localstorage 中是否存在驗證碼
+        let verifyCode = localStorage.getItem("authorization");
+
+        // 如果本地存儲中存在驗證碼，則跳轉到會員資料畫面
+        if (verifyCode) {
+            window.location.href = "/member/detail";
+        } else {
+            // 如果本地存儲中不存在驗證碼，則跳轉到登入畫面
+            window.location.href = "/member/login";
+        }
+    });
 });
 
 // 登出按鈕的點擊事件
 $("#sign-out").click(function () {
+    localStorage.removeItem("authorization");
+});
+
+// 行動版登出按鈕的點擊事件
+$("#sign-out-android").click(function () {
     localStorage.removeItem("authorization");
 });
