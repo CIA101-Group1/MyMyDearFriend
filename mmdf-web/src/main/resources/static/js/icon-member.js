69f74fb5cddd7112ea7e100794e41a604664fe86
peihui <pride97350@gmail.com>
2024-05-14 23:29:31 +0800

    前台：更改api-response-utils.js的方法 讓前端統一由監聽器處理需登入但未登入，要轉到登入畫面的導轉

--- mmdf-web/src/main/resources/static/js/icon-member.js
+++ mmdf-web/src/main/resources/static/js/icon-member.js
@@ -14,23 +14,8 @@ $(document).ready(function () {
         $("#sign-out-android").hide();
     }
 
-    // 點擊人頭icon時觸發事件
-    $("#icon-User").click(function (e) {
-        e.preventDefault();
-        // 檢查 localstorage 中是否存在驗證碼
-        let verifyCode = localStorage.getItem("authorization");
-
-        // 如果本地存儲中存在驗證碼，則跳轉到會員資料畫面
-        if (verifyCode) {
-            window.location.href = "/member/detail";
-        } else {
-            // 如果本地存儲中不存在驗證碼，則跳轉到登入畫面
-            window.location.href = "/member/login";
-        }
-    });
-
-    // 行動版點擊人頭icon時觸發事件
-    $("#icon-User-android").click(function (e) {
+    // 點擊人頭icon、"查看/修改會員資料"按鈕時觸發事件
+    $("#icon-User, #icon-User-android, #member-detail, #member-detail-android").click(function (e) {
         e.preventDefault();
         // 檢查 localstorage 中是否存在驗證碼
         let verifyCode = localStorage.getItem("authorization");
