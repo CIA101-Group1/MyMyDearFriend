21730c47e723f31ed21c7f79b867d8b73e9108a6
Nueve <tk25683@gmail.com>
2024-05-22 18:30:20 +0800

    增加detailByID方法

--- mmdf-admin/src/main/java/com/tibame/group1/admin/service/EmployeeService.java
+++ mmdf-admin/src/main/java/com/tibame/group1/admin/service/EmployeeService.java
@@ -5,6 +5,7 @@ import com.tibame.group1.common.dto.web.LoginResDTO;
 import com.tibame.group1.common.exception.CheckRequestErrorException;
 import com.tibame.group1.common.exception.DateException;
 
+
 import java.io.IOException;
 import java.util.List;
 
@@ -14,8 +15,20 @@ public interface EmployeeService {
             EmployeeCreateReqDTO req, AdminLoginSourceDTO adminLoginSource)
             throws CheckRequestErrorException, IOException, DateException;
 
+    /**
+     * 登入員工看自己資訊
+     *
+     * @param adminLoginSource
+     * @return
+     * @throws CheckRequestErrorException
+     * @throws IOException
+     */
     EmployeeDetailResDTO employeeDetail(AdminLoginSourceDTO adminLoginSource)
             throws CheckRequestErrorException, IOException;
+
+    EmployeeDetailResDTO employeeDetailById(AdminLoginSourceDTO adminLoginSource, Integer employeeId)
+        throws CheckRequestErrorException;
+
 
     EmployeeEditResDTO employeeEdit(EmployeeEditReqDTO req, AdminLoginSourceDTO adminLoginSource)
             throws CheckRequestErrorException, IOException;
