b50d8e13ce925935529729f8ed93c733ccca10ec
HaoXinag <karta0128@gmail.com>
2024-05-23 23:09:32 +0800

    新增 -> 客服聊天室、聊天室初始、聊天室對話  WebSocket設定

---
+++ mmdf-web/src/main/java/com/tibame/group1/web/config/WebSocketConfig.java
@@ -0,0 +1,38 @@
+package com.tibame.group1.web.config;
+
+import com.tibame.group1.web.service.impl.webSocketMemberChatInit;
+import com.tibame.group1.web.service.impl.webSocketHelperService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+import com.tibame.group1.web.service.impl.ChatWebCocketServiceImpl;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(webSocketHandler(), "/message").setAllowedOrigins("*");
+        registry.addHandler(webSocketService(), "/service").setAllowedOrigins("*");
+        registry.addHandler(webSocketMemberChatInit(), "/chatroom").setAllowedOrigins("*");
+    }
+    //聊天室功能
+    @Bean
+    public WebSocketHandler webSocketHandler() {
+        return new ChatWebCocketServiceImpl();
+    }
+    //客服聊天室
+    @Bean
+    public WebSocketHandler webSocketService() {
+        return new webSocketHelperService();
+    }
+    //初始化聊天室
+    @Bean WebSocketHandler webSocketMemberChatInit() {
+        return new webSocketMemberChatInit();
+    }
+}
