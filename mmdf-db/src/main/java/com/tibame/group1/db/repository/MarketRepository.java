b6eb56dcbcd25cc4b28a2ae562424c468dcb13a2
Nueve <tk25683@gmail.com>
2024-05-24 10:49:50 +0800

    增加查詢方法

--- mmdf-db/src/main/java/com/tibame/group1/db/repository/MarketRepository.java
+++ mmdf-db/src/main/java/com/tibame/group1/db/repository/MarketRepository.java
@@ -3,5 +3,8 @@ package com.tibame.group1.db.repository;
 import com.tibame.group1.db.entity.MarketEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MarketRepository extends JpaRepository<MarketEntity, Integer> {
+    List<MarketEntity> findByMarketStatus(Integer marketStatus);
 }
