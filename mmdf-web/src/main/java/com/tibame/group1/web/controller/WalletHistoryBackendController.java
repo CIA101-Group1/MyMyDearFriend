f8431a58de82015689756bad445996451a180ed1
tcb119 <kai_sunny@icloud.com>
2024-05-26 17:02:29 +0800

    前台：錢包新增功能(spring JPA版) 可以對錢包使用：(+$)儲值，入帳 / (-$)手續費，付款，市集報名費，提款

--- mmdf-web/src/main/java/com/tibame/group1/web/controller/WalletHistoryBackendController.java
+++ mmdf-web/src/main/java/com/tibame/group1/web/controller/WalletHistoryBackendController.java
@@ -107,7 +107,7 @@ public class WalletHistoryBackendController {
   //        return ResponseEntity.status(HttpStatus.CREATED).body(wallet);
   //    }
 
-
+  /** todo: 錢包分頁功能與每筆細項，用List */
   @CheckLogin
   @GetMapping("/wallets")
   public ResponseEntity<List<WalletHistoryEntity>> getAllWalletHistory(
