866ff53f22094c2ef45be3e5999662a0415b38f3
tcb119 <kai_sunny@icloud.com>
2024-05-26 16:53:13 +0800

    前台：錢包新增功能(spring JPA版)， 可以對錢包使用：(+$)儲值，入帳 (-$)手續費，付款，市集報名費，提款

--- mmdf-common/src/main/java/com/tibame/group1/common/enums/WalletCategory.java
+++ mmdf-common/src/main/java/com/tibame/group1/common/enums/WalletCategory.java
@@ -3,50 +3,28 @@ package com.tibame.group1.common.enums;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-/**
- * todo 是否要加上金錢顯示
- */
-
-
+/** todo 是否要加上金錢顯示 */
 @AllArgsConstructor
 @Getter
 public enum WalletCategory {
+  PAYMENT(0, "付款完成"),
 
-    PAYMENT("0", "付款完成"),
+  TOP_UP(1, "儲值成功"),
 
-    TOP_UP("1", "儲值成功"),
+  WITHDRAW(2, "提款成功"),
 
-    WITHDRAW("2", "提款成功"),
+  REFUND(3, "退款完成"),
 
-    REFUND("3", "退款完成"),
+  DEPOSIT(4, "入帳成功"),
 
-    DEPOSIT("4", "入帳成功"),
+  FEE(5, "扣手續費"),
 
-    FEE("5", "扣手續費"),
+  TEST(6, "測試使用"),
 
-    TEST("6", "測試使用"),
+  MARKET(7, "市集報名費"),
 
-    MARKET("7", "市集報名費"),
+  WITHDRAWAL(8, "XXX");
 
-    WITHDRAWAL("8", "XXX");
-
-    private final String code;
-    private final String message;
-
-    public String getCode() {
-        return code;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public static WalletCategory fromCode(String code) {
-        for (WalletCategory category : WalletCategory.values()) {
-            if (category.getCode().equals(code)) {
-                return category;
-            }
-        }
-        throw new IllegalArgumentException("無效的代碼: " + code);
-    }
+  private final int code;
+  private final String message;
 }
--- mmdf-db/src/main/java/com/tibame/group1/db/dao/impl/WalletHistoryDAOImpl.java
+++ mmdf-db/src/main/java/com/tibame/group1/db/dao/impl/WalletHistoryDAOImpl.java
@@ -1,16 +1,14 @@
 package com.tibame.group1.db.dao.impl;
 
-import com.tibame.group1.common.enums.WalletCategory;
 import com.tibame.group1.db.dao.WalletHistoryDAO;
 import com.tibame.group1.db.dto.WalletQueryParams;
-import com.tibame.group1.db.dto.WalletReqDTO;
+
 import com.tibame.group1.db.rowmapper.WalletRowMapperUtils;
 import com.tibame.group1.db.entity.WalletHistoryEntity;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
+
 import org.springframework.stereotype.Component;
 
 import java.util.*;
@@ -23,14 +21,14 @@ public class WalletHistoryDAOImpl implements WalletHistoryDAO {
   @Override
   public WalletHistoryEntity getWalletHistoryById(Integer walletHistoryId) {
     String sql =
-            "SELECT wallet_history_id, change_time, member_id, change_amount, change_type, wallet_amount\n"
-                    + "FROM wallet_history WHERE wallet_history_id = :wallet_history_id";
+        "SELECT wallet_history_id, change_time, member_id, change_amount, change_type, wallet_amount\n"
+            + "FROM wallet_history WHERE wallet_history_id = :wallet_history_id";
 
     Map<String, Object> map = new HashMap<>();
     map.put("wallet_history_id", walletHistoryId);
 
     List<WalletHistoryEntity> walletHistoryEntityList =
-            namedParameterJdbcTemplate.query(sql, map, new WalletRowMapperUtils());
+        namedParameterJdbcTemplate.query(sql, map, new WalletRowMapperUtils());
 
     if (walletHistoryEntityList.size() > 0) {
       return walletHistoryEntityList.get(0);
@@ -39,73 +37,81 @@ public class WalletHistoryDAOImpl implements WalletHistoryDAO {
     }
   }
 
-  @Override
-  public Integer createWalletHistory(WalletReqDTO walletReqDTO) {
-    // 計算新的 walletAmount
-    Integer currentWalletAmount = getCurrentWalletAmount(walletReqDTO.getMemberID());
-    Integer newWalletAmount = calculateNewWalletAmount(currentWalletAmount, walletReqDTO.getChangeAmount(), walletReqDTO.getChangeType());
-
-    String sql =
-            "INSERT INTO wallet_history (change_time, member_id, change_amount, change_type, wallet_amount) "
-                    + "VALUES (:changeTime, :memberID, :changeAmount, :changeType, :walletAmount)";
-
-    Map<String, Object> map = new HashMap<>();
-    map.put("memberID", walletReqDTO.getMemberID());
-    map.put("changeAmount", walletReqDTO.getChangeAmount());
-    map.put("changeType", walletReqDTO.getChangeType().toString());
-    map.put("walletAmount", newWalletAmount);
-
-    Date now = new Date();
-    map.put("changeTime", now);
-
-    KeyHolder keyHolder = new GeneratedKeyHolder();
-    namedParameterJdbcTemplate.update(sql, new MapSqlParameterSource(map), keyHolder);
-
-    int walletID = keyHolder.getKey().intValue();
-    return walletID;
-  }
-
-  private Integer calculateNewWalletAmount(Integer currentWalletAmount, Integer changeAmount, WalletCategory changeType) {
-    switch (changeType) {
-      case TOP_UP:
-      case DEPOSIT:
-        return currentWalletAmount + changeAmount;
-      case PAYMENT:
-      case WITHDRAW:
-      case FEE:
-      case MARKET:
-        return currentWalletAmount - changeAmount;
-      default:
-        throw new IllegalArgumentException("無效的變更類型: " + changeType);
-    }
-  }
-
-  private Integer getCurrentWalletAmount(Integer memberID) {
-    String sql = "SELECT wallet_amount FROM wallet_history WHERE member_id = :memberID ORDER BY change_time DESC LIMIT 1";
-
-    Map<String, Object> map = new HashMap<>();
-    map.put("memberID", memberID);
-
-    List<Integer> walletAmounts = namedParameterJdbcTemplate.queryForList(sql, map, Integer.class);
-
-    if (!walletAmounts.isEmpty()) {
-      return walletAmounts.get(0);
-    } else {
-      return 0;
-    }
-  }
+  //  @Override
+  //  public Integer createWalletHistory(WalletReqDTO walletReqDTO) {
+  //    // 計算新的 walletAmount
+  //    Integer currentWalletAmount = getCurrentWalletAmount(walletReqDTO.getMemberID());
+  //    Integer newWalletAmount = calculateNewWalletAmount(currentWalletAmount,
+  // walletReqDTO.getChangeAmount(), walletReqDTO.getChangeType());
+  //
+  //    String sql =
+  //        "INSERT INTO wallet_history (member_id, change_amount, change_type, change_time,
+  // wallet_amount)\n"
+  //            + "SELECT wh.member_id, :changeAmount, :changeType, :changeTime, m.wallet_amount\n"
+  //            + "FROM wallet_history wh\n"
+  //            + "INNER JOIN member m ON wh.member_id = m.member_id\n"
+  //            + "WHERE wh.member_id = :memberID";
+  //
+  //    Map<String, Object> map = new HashMap<>();
+  //    map.put("memberID", walletReqDTO.getMemberID());
+  //    map.put("changeAmount", walletReqDTO.getChangeAmount());
+  //    map.put("changeType", walletReqDTO.getChangeType().getCode());
+  //
+  //
+  //
+  //    Date now = new Date();
+  //    map.put("changeTime", now);
+  //
+  //    KeyHolder keyHolder = new GeneratedKeyHolder();
+  //    namedParameterJdbcTemplate.update(sql, new MapSqlParameterSource(map), keyHolder);
+  //
+  //    int walletID = keyHolder.getKey().intValue();
+  //    return walletID;
+  //  }
+  //
+  //  private Integer calculateNewWalletAmount(Integer currentWalletAmount, Integer changeAmount,
+  // WalletCategory changeType) {
+  //    switch (changeType) {
+  //      case TOP_UP:
+  //      case DEPOSIT:
+  //        return currentWalletAmount + changeAmount;
+  //      case PAYMENT:
+  //      case WITHDRAW:
+  //      case FEE:
+  //      case MARKET:
+  //        return currentWalletAmount - changeAmount;
+  //      default:
+  //        throw new IllegalArgumentException("無效的變更類型: " + changeType);
+  //    }
+  //  }
+  //
+  //  private Integer getCurrentWalletAmount(Integer memberID) {
+  //    String sql = "SELECT wallet_amount FROM wallet_history WHERE member_id = :memberID ORDER BY
+  // change_time DESC LIMIT 1";
+  //
+  //    Map<String, Object> map = new HashMap<>();
+  //    map.put("memberID", memberID);
+  //
+  //    List<Integer> walletAmounts = namedParameterJdbcTemplate.queryForList(sql, map,
+  // Integer.class);
+  //
+  //    if (!walletAmounts.isEmpty()) {
+  //      return walletAmounts.get(0);
+  //    } else {
+  //      return 0;
+  //    }
+  //  }
 
   @Override
   public List<WalletHistoryEntity> getWallets(WalletQueryParams walletQueryParams) {
     String sql =
-            "SELECT wallet_history_id, change_time, member_id, change_amount, change_type, wallet_amount " +
-                    "FROM wallet_history WHERE 1=1 ";
+        "SELECT wallet_history_id, change_time, member_id, change_amount, change_type, wallet_amount "
+            + "FROM wallet_history WHERE 1=1 ";
 
     Map<String, Object> map = new HashMap<>();
 
     sql = sql + " AND member_id = :memberId ";
-    map.put("memberId",walletQueryParams.getMemberId());
-
+    map.put("memberId", walletQueryParams.getMemberId());
 
     if (walletQueryParams.getWalletCategory() != null) {
       sql = sql + " AND change_type = :changeType";
@@ -118,7 +124,7 @@ public class WalletHistoryDAOImpl implements WalletHistoryDAO {
     }
 
     List<WalletHistoryEntity> walletHistoryEntityList =
-            namedParameterJdbcTemplate.query(sql, map, new WalletRowMapperUtils());
+        namedParameterJdbcTemplate.query(sql, map, new WalletRowMapperUtils());
 
     return walletHistoryEntityList;
   }
