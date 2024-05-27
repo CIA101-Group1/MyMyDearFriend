e73d2e320c0b95a9a752dc0d33987696177840cd
tcb119 <kai_sunny@icloud.com>
2024-05-27 17:44:43 +0800

    後台：錢包審核，新增WalletRequestEntity

--- mmdf-common/src/main/java/com/tibame/group1/common/enums/WalletCategory.java
+++ mmdf-common/src/main/java/com/tibame/group1/common/enums/WalletCategory.java
@@ -3,35 +3,34 @@ package com.tibame.group1.common.enums;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-/** todo 是否要加上金錢顯示 */
 @AllArgsConstructor
 @Getter
 public enum WalletCategory {
-    PAYMENT(0, "付款"),
+  PAYMENT(0, "付款"),
 
-    TOP_UP(1, "儲值"),
+  TOP_UP(1, "儲值"),
 
-    WITHDRAW(2, "提款"),
+  WITHDRAW(2, "提款"),
 
-    REFUND(3, "退款"),
+  REFUND(3, "退款"),
 
-    DEPOSIT(4, "入帳"),
+  DEPOSIT(4, "入帳"),
 
-    FEE(5, "手續費"),
+  FEE(5, "手續費"),
 
-    MARKET(6, "市集報名費");
+  MARKET(6, "市集報名費");
 
-    private final int code;
+  private final int code;
 
-    private final String message;
+  private final String message;
 
-    // 根據代碼查找對應的enum值
-    public static WalletCategory fromCode(int code) {
-        for (WalletCategory category : WalletCategory.values()) {
-            if (category.getCode() == code) {
-                return category;
-            }
-        }
-        throw new IllegalArgumentException("無效的代碼: " + code);
+  // 根據代碼查找對應的enum值
+  public static WalletCategory fromCode(int code) {
+    for (WalletCategory category : WalletCategory.values()) {
+      if (category.getCode() == code) {
+        return category;
+      }
     }
+    throw new IllegalArgumentException("無效的代碼: " + code);
+  }
 }
