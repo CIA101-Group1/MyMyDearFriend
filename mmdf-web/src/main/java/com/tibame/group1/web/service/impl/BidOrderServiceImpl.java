5482b7458cbc78481c9b404753dcbc8ec0241f63
Chaonew <chaoneww@gmail.com>
2024-05-29 05:02:51 +0800

    前台：修改競標訂單結帳、出貨、完成功能

--- mmdf-web/src/main/java/com/tibame/group1/web/service/impl/BidOrderServiceImpl.java
+++ mmdf-web/src/main/java/com/tibame/group1/web/service/impl/BidOrderServiceImpl.java
@@ -3,16 +3,14 @@ package com.tibame.group1.web.service.impl;
 import com.tibame.group1.common.dto.web.BidOrderPayReqDTO;
 import com.tibame.group1.common.enums.BidOrderStatus;
 import com.tibame.group1.common.exception.CheckRequestErrorException;
-import com.tibame.group1.db.entity.BidEntity;
-import com.tibame.group1.db.entity.BidOrderEntity;
-import com.tibame.group1.db.entity.BidProductEntity;
-import com.tibame.group1.db.entity.MemberEntity;
+import com.tibame.group1.db.entity.*;
 import com.tibame.group1.db.repository.BidOrderRepository;
 import com.tibame.group1.db.repository.MemberRepository;
 import com.tibame.group1.web.dto.LoginSourceDTO;
 import com.tibame.group1.web.dto.WalletCreateReqDTO;
 import com.tibame.group1.web.dto.WalletCreateResDTO;
 import com.tibame.group1.web.service.BidOrderService;
+import com.tibame.group1.web.service.NoticeService;
 import com.tibame.group1.web.service.WalletHistoryService;
 
 import jakarta.transaction.Transactional;
@@ -33,6 +31,7 @@ public class BidOrderServiceImpl implements BidOrderService {
     @Autowired BidOrderRepository bidOrderRepository;
     @Autowired MemberRepository memberRepository;
     @Autowired WalletHistoryService walletHistoryService;
+    @Autowired NoticeService noticeService;
 
     @Override
     public void createOrderForHighestBid(BidProductEntity product, Optional<BidEntity> highestBid) {
@@ -74,10 +73,56 @@ public class BidOrderServiceImpl implements BidOrderService {
                 bidOrderRepository
                         .findById(orderId)
                         .orElseThrow(() -> new CheckRequestErrorException("查無此競標訂單"));
+        // 出貨
+        if (newStatus.equals(BidOrderStatus.DELIVERED)) {
+            // 更新訂單狀態
+            order.setStatus(newStatus);
+            order.setUpdateTime(Timestamp.from(Instant.now()));
+            bidOrderRepository.save(order);
+            // 發送通知給買家
+            noticeService.memberNoticeCreate(
+                    memberRepository.findById(order.getBuyerId()).get(),
+                    MemberNoticeEntity.NoticeCategory.BID_PRODUCT,
+                    "競標訂單出貨",
+                    "您的競標訂單：" + "編號" + order.getOrderId() + " 賣家已出貨",
+                    false);
+        }
+        // 完成訂單
+        else if (newStatus.equals(BidOrderStatus.COMPLETED)) {
+            // 新增賣家錢包入帳紀錄
+            WalletCreateReqDTO walletCreateReqDTO = new WalletCreateReqDTO();
+            walletCreateReqDTO.setWalletCategory("4");
+            walletCreateReqDTO.setChangeAmount(order.getSubtotal() - order.getFee());
 
-        order.setStatus(newStatus);
-        order.setUpdateTime(Timestamp.from(Instant.now()));
-        bidOrderRepository.save(order);
+            LoginSourceDTO sellerLoginSource = new LoginSourceDTO();
+            sellerLoginSource.setMemberId(order.getSellerId());
+
+            WalletCreateResDTO walletCreateResDTO =
+                    walletHistoryService.walletHistoryCreate(walletCreateReqDTO, sellerLoginSource);
+            // 餘額不足
+            if (Objects.equals(
+                    walletCreateResDTO.getStatus(),
+                    WalletCreateResDTO.Status.INSUFFICIENT_BALANCE.getCode())) {
+                throw new CheckRequestErrorException(
+                        WalletCreateResDTO.Status.INSUFFICIENT_BALANCE.getMessage());
+            }
+            // 更新訂單狀態
+            if (Objects.equals(
+                    walletCreateResDTO.getStatus(),
+                    WalletCreateResDTO.Status.CREATE_SUCCESS.getCode())) {
+                // 更新訂單狀態
+                order.setStatus(newStatus);
+                order.setUpdateTime(Timestamp.from(Instant.now()));
+                bidOrderRepository.save(order);
+                // 發送通知給賣家
+                noticeService.memberNoticeCreate(
+                        memberRepository.findById(order.getSellerId()).get(),
+                        MemberNoticeEntity.NoticeCategory.BID_PRODUCT,
+                        "競標訂單已完成",
+                        "您的競標訂單：" + "編號" + order.getOrderId() + " 買家已完成訂單，平台將進行撥款",
+                        false);
+            }
+        }
     }
 
     @Override
@@ -123,6 +168,13 @@ public class BidOrderServiceImpl implements BidOrderService {
             order.setFee((int) Math.round(order.getSubtotal() * 0.03));
             order.setUpdateTime(Timestamp.from(Instant.now()));
             bidOrderRepository.save(order);
+            // 發送通知給賣家
+            noticeService.memberNoticeCreate(
+                    memberRepository.findById(order.getSellerId()).get(),
+                    MemberNoticeEntity.NoticeCategory.BID_PRODUCT,
+                    "競標訂單已付款",
+                    "您的競標訂單：" + "編號" + order.getOrderId() + " 買家已付款，請準備出貨",
+                    false);
         }
     }
 
