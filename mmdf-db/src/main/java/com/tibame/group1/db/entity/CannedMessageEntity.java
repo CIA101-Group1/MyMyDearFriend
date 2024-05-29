1c28da818bce9d4467a8ba12266c28dc787865f4
HaoXinag <karta0128@gmail.com>
2024-05-29 12:23:02 +0800

    新增 -> 罐頭訊息實作和資料庫傳送

--- mmdf-db/src/main/java/com/tibame/group1/db/entity/CannedMessageEntity.java
+++ mmdf-db/src/main/java/com/tibame/group1/db/entity/CannedMessageEntity.java
@@ -1,2 +1,19 @@
-package com.tibame.group1.db.entity;public class cannedMessageEntity {
+package com.tibame.group1.db.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+@Entity
+@Getter
+@Setter
+@Table(name="canned_message")
+public class CannedMessageEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "messageId")
+    private Integer messageId;
+    @Column(name="serviceId")
+    private Integer serviceId;
+    @Column(name="message")
+    private String message;
 }
