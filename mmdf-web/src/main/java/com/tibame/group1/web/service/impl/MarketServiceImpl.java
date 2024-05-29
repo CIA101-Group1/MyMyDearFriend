38997560735818b15f75c42f03c2682ecd7e4737
Nueve <tk25683@gmail.com>
2024-05-29 12:41:09 +0800

    前台：市集新增錢包扣款及退款功能、報名人數達上限無法報名

--- mmdf-web/src/main/java/com/tibame/group1/web/service/impl/MarketServiceImpl.java
+++ mmdf-web/src/main/java/com/tibame/group1/web/service/impl/MarketServiceImpl.java
@@ -1,19 +1,20 @@
 package com.tibame.group1.web.service.impl;
 
+import com.tibame.group1.common.enums.WalletCategory;
 import com.tibame.group1.common.exception.CheckRequestErrorException;
 import com.tibame.group1.common.utils.ConvertUtils;
 import com.tibame.group1.common.utils.DateUtils;
-import com.tibame.group1.db.entity.MarketEntity;
-import com.tibame.group1.db.entity.MarketRegistrationEntity;
-import com.tibame.group1.db.entity.MemberEntity;
-import com.tibame.group1.db.entity.MemberNoticeEntity;
+import com.tibame.group1.db.entity.*;
 import com.tibame.group1.db.repository.MarketRegistrationRepository;
 import com.tibame.group1.db.repository.MarketRepository;
 import com.tibame.group1.db.repository.MemberRepository;
+import com.tibame.group1.db.repository.WalletHistoryRepository;
 import com.tibame.group1.web.dto.*;
 import com.tibame.group1.web.service.MarketService;
-
 import com.tibame.group1.web.service.NoticeService;
+
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,6 +25,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 public class MarketServiceImpl implements MarketService {
 
     @Autowired private MarketRepository marketRepository;
@@ -33,6 +35,8 @@ public class MarketServiceImpl implements MarketService {
     @Autowired private MemberRepository memberRepository;
 
     @Autowired private NoticeService noticeService;
+
+    @Autowired private WalletHistoryRepository walletHistoryRepository;
 
 
     //狀態為2的市集顯示在前台畫面
@@ -177,17 +181,43 @@ public class MarketServiceImpl implements MarketService {
                 market.getApplicantPopulation() != null ? market.getApplicantPopulation() : 0;
         int applicantLimit = market.getApplicantLimit();
         if (applicationPopulation >= applicantLimit){
-            throw new CheckRequestErrorException("報名人數已達上限，無法報名成功");
-        }
-        market.setApplicantPopulation(applicationPopulation + 1);
-        marketRepository.save(market);
+            log.warn("報名人數已達上限");
+            throw new CheckRequestErrorException("報名人數，已達上限");
+        } else {
+            market.setApplicantPopulation(applicationPopulation + 1);
+            marketRepository.save(market);
 
-        // 创建并返回 MemberRegistrationResDTO
-        MemberRegistrationResDTO registrationResDTO = new MemberRegistrationResDTO();
-        registrationResDTO.setMarketId(marketRegistrationReq.getMarketId());
-        registrationResDTO.setMemberId(loginSource.getMemberId());
-        noticeService.memberNoticeCreate(marketRegistrationEntity.getMemberId(), MemberNoticeEntity.NoticeCategory.MARKET, "市集報名","恭喜市集報名成功");
-        return registrationResDTO;
+            Integer sellerWalletAmount = memberEntity.getWalletAmount();
+
+            // 賣家錢包餘額計算
+            if (sellerWalletAmount < market.getMarketFee()) {
+                log.warn("賣家錢包餘額不足");
+                throw new CheckRequestErrorException("錢包餘額不足，請先儲值");
+            } else {
+                memberEntity.setWalletAmount(sellerWalletAmount - market.getMarketFee());
+                memberRepository.save(memberEntity);
+            }
+
+            // 創建賣家的 WalletHistoryEntity
+            WalletHistoryEntity sellerWalletHistory = new WalletHistoryEntity();
+            sellerWalletHistory.setMember(memberEntity); // 設置會員ID
+            sellerWalletHistory.setChangeAmount(
+                    memberEntity.getWalletAmount() - market.getMarketFee());
+            sellerWalletHistory.setChangeTime(new Date());
+            sellerWalletHistory.setChangeType(WalletCategory.MARKET);
+            walletHistoryRepository.save(sellerWalletHistory);
+
+            // 创建并返回 MemberRegistrationResDTO
+            MemberRegistrationResDTO registrationResDTO = new MemberRegistrationResDTO();
+            registrationResDTO.setMarketId(marketRegistrationReq.getMarketId());
+            registrationResDTO.setMemberId(loginSource.getMemberId());
+            noticeService.memberNoticeCreate(
+                    marketRegistrationEntity.getMemberId(),
+                    MemberNoticeEntity.NoticeCategory.MARKET,
+                    "市集報名",
+                    "恭喜市集報名成功");
+            return registrationResDTO;
+        }
     }
 
     //取消報名
@@ -222,6 +252,20 @@ public class MarketServiceImpl implements MarketService {
             }
         }
         marketRepository.save(marketEntity);
+
+        // 賣家錢包增加金額
+        Integer sellerWalletAmount = memberEntity.getWalletAmount();
+        memberEntity.setWalletAmount(sellerWalletAmount + marketEntity.getMarketFee());
+        memberRepository.save(memberEntity);
+
+
+        // 創建賣家的 WalletHistoryEntity
+        WalletHistoryEntity sellerWalletHistory = new WalletHistoryEntity();
+        sellerWalletHistory.setMember(memberEntity); // 設置會員ID
+        sellerWalletHistory.setChangeAmount(memberEntity.getWalletAmount() + marketEntity.getMarketFee());
+        sellerWalletHistory.setChangeTime(new Date());
+        sellerWalletHistory.setChangeType(WalletCategory.REFUND);
+        walletHistoryRepository.save(sellerWalletHistory);
 
         MarketCancelResDTO dto = new MarketCancelResDTO();
         dto.setStatus(registration.getStatus());
